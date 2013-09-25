221d259034db2ef1069451f1aac4004e31c68af4
Akshay Karle <akshay.a.karle@gmail.com>
2013-09-25 23:36:06 +0530

    Get the basics working

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,7 @@
+class ProjectsController < ApplicationController
+  def index
+    server_config_json = JSON.parse(File.read('config/build_server_config.json'))
+    c = CiStatus::CruiseControl.new(server_config_json['server_url'])
+    @projects = c.projects
+  end
+end
