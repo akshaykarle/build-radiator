6623c6baa572b6e494920496124b4d9ff07ce139
Akshay Karle <akshay.a.karle@gmail.com>
2013-09-25 23:48:22 +0530

    Sort builds by the build status and make the radiator pretty.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,6 @@ class ProjectsController < ApplicationController
   def index
     server_config_json = JSON.parse(File.read('config/build_server_config.json'))
     c = CiStatus::CruiseControl.new(server_config_json['server_url'])
-    @projects = c.projects
+    @projects = c.projects.sort_by {|project| project.status }
   end
 end
